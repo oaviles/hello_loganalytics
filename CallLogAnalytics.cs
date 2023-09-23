a57144c7ab3d2a8d6af50e74f4528442331f0968
Omar Aviles <oaviles@live.com>
2023-09-23 07:21:29 -0600

    Initial Commit

---
+++ CallLogAnalytics.cs
@@ -0,0 +1,50 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Azure.Identity;
+using Azure.Monitor.Query;
+using Azure.Monitor.Query.Models;
+
+namespace Company.Function
+{
+    public static class CallLogAnalytics
+    {
+        [FunctionName("CallLogAnalytics")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            var tenantId =  Environment.GetEnvironmentVariable("TENANT_ID"); 
+            var clientId =  Environment.GetEnvironmentVariable("API_ID");
+            var clientSecret =  Environment.GetEnvironmentVariable("API_KEY"); 
+            var workspaceId =  Environment.GetEnvironmentVariable("WORKSPACE_ID");
+            var query = Environment.GetEnvironmentVariable("QUERY"); 
+
+            var credential = new ClientSecretCredential(tenantId, clientId, clientSecret);
+            var client = new LogsQueryClient(credential);
+
+            try
+            {
+
+                var result = await client.QueryWorkspaceAsync(workspaceId,query,new QueryTimeRange(TimeSpan.FromDays(1)));
+
+                log.LogInformation("Call LogAnalytics: " + result.Value.Table.Rows.Count + " rows returned");
+
+                return new OkObjectResult(result);
+            }
+            catch (Exception ex)
+            {
+                log.LogError(ex.Message);
+                return new BadRequestObjectResult(ex.Message);
+            }
+        }
+    }
+}
