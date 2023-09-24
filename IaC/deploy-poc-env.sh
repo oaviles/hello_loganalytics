d9acd62441e569f73b345c6b357105c0ceede13d
Omar Aviles <oaviles@live.com>
2023-09-24 05:30:27 -0600

    Update deploy-poc-env.sh

--- IaC/deploy-poc-env.sh
+++ IaC/deploy-poc-env.sh
@@ -10,4 +10,4 @@ az group create --name $RESOURCE_GROUP --location $REGION_NAME
 
 az storage account create --name $STORAGE_NAME --resource-group $RESOURCE_GROUP --location $REGION_NAME --sku Standard_LRS
 
-az functionapp create --resource-group $RESOURCE_GROUP --name $FUNCTION_NAME --os-type Windows --runtime dotnet --runtime-version 6 --functions-version 4 --storage-account $STORAGE_NAME
+az functionapp create --resource-group $RESOURCE_GROUP --name $FUNCTION_NAME --os-type Windows --runtime dotnet --runtime-version 6 --functions-version 4 --storage-account $STORAGE_NAME --consumption-plan-location $REGION_NAME
