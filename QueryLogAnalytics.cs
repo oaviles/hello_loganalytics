497698a08c3f60850ae0201c629c382972532e51
Omar Aviles <oaviles@live.com>
2023-09-25 10:51:45 -0600

    New fcuntion passing quey in body

---
+++ QueryLogAnalytics.cs
@@ -0,0 +1,57 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Azure.Identity;
+using Azure.Monitor.Query;
+using Azure.Monitor.Query.Models;
+
+namespace Company.Function
+{
+    public static class QueryLogAnalytics
+    {
+        [FunctionName("QueryLogAnalytics")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string query = req.Query["query"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            query = query ?? data?.query;
+
+            var tenantId =  Environment.GetEnvironmentVariable("TENANT_ID"); 
+            var clientId =  Environment.GetEnvironmentVariable("API_ID");
+            var clientSecret =  Environment.GetEnvironmentVariable("API_KEY"); 
+            var workspaceId =  Environment.GetEnvironmentVariable("WORKSPACE_ID");
+            // var query = Environment.GetEnvironmentVariable("QUERY"); 
+            var days = Environment.GetEnvironmentVariable("DAYS"); 
+
+            var credential = new ClientSecretCredential(tenantId, clientId, clientSecret);
+            var client = new LogsQueryClient(credential);
+
+            try
+            {
+
+                var result = await client.QueryWorkspaceAsync(workspaceId, query, new QueryTimeRange(TimeSpan.FromDays(double.Parse(days))));
+
+                log.LogInformation("Call LogAnalytics: " + result.Value.Table.Rows.Count + " rows returned");
+
+                return new OkObjectResult(result);
+            }
+            catch (Exception ex)
+            {
+                log.LogError(ex.Message);
+                return new BadRequestObjectResult(ex.Message);
+            }
+        }
+    }
+}
